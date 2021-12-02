ce096d304b455fd2c0381fa45b2777344a820fae
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-02 14:24:39 +0100

    Add files via upload

---
+++ Resources/js/index.js
@@ -0,0 +1,4 @@
+import fieldRegistry from '../../../../sulu/sulu/src/Sulu/Bundle/AdminBundle/Resources/js/containers/Form/registries/fieldRegistry.js';
+import ColorPickerCustomTest from './components/ColorPickerCustom/ColorPickerCustomTest';
+
+fieldRegistry.add('color_picker_custom_test', ColorPickerCustomTest);
