742d09ca68fc3ee144bef731ec70d673a22109c0
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-03 14:27:03 +0100

    Update index.js

--- src/Resources/js/index.js
+++ src/Resources/js/index.js
@@ -1,4 +1,4 @@
-import fieldRegistry from '../../../../../sulu/sulu/src/Sulu/Bundle/AdminBundle/Resources/js/containers/Form/registries/fieldRegistry.js';
+import { fieldRegistry } from 'sulu-admin-bundle/containers'
 import ColorPickerCustomTest from './components/ColorPickerCustom/ColorPickerCustomTest';
 
 fieldRegistry.add('color_picker_custom_test', ColorPickerCustomTest);
