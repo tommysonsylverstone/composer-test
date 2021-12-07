a8142977b595763ce4a90ff8f1eae10860ff8263
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-07 17:11:24 +0100

    Add files via upload

--- src/ColorPickerCustomBundle.php
+++ src/ColorPickerCustomBundle.php
@@ -2,11 +2,12 @@
 
 namespace Tommysonsylverstone\ComposerTest;
 
-use Sulu\Bundle\AdminBundle\DependencyInjection\Compiler\AddAdminPass;
-use Sulu\Bundle\AdminBundle\DependencyInjection\Compiler\AddMetadataProviderPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ColorPickerCustomBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\CategoryBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('composer_test');
+        $rootNode = $treeBuilder->getRootNode();
+
+        $this->addObjectsSection($rootNode);
+
+        return $treeBuilder;
+    }
+
+    /**
+     * Adds `objects` section.
+     */
+    private function addObjectsSection(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->arrayNode('objects')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('category')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Tommysonsylverstone\ComposerTest\Content\Types\ColorPickerCustomTest')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+}
