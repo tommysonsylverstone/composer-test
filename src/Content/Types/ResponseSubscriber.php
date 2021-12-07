6881074d2f175c05b5fd1a8e15cb2ce0b74d2c4c
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-07 17:27:56 +0100

    Add files via upload

---
+++ src/Content/Types/ResponseSubscriber.php
@@ -0,0 +1,25 @@
+
+<?php
+namespace Tommysonsylverstone\ComposerTest\Content\Types;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ResponseSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => [
+                ['addSecurityHeaders', 0],
+            ],
+        ];
+    }
+
+    public function addSecurityHeaders(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $response->headers->set('X-Header-Set-By', 'My custom bundle');
+    }
+}
