d890b671d486a63eea0b555ceca0ec1849b70311
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-07 17:29:14 +0100

    Update ColorPickerCustomBundle.php

--- src/ColorPickerCustomBundle.php
+++ src/ColorPickerCustomBundle.php
@@ -6,8 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ColorPickerCustomBundle extends Bundle
 {
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
+
 }
