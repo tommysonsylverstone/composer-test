fe605d884367a169c2522b2e0c980782c092edcb
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-08 13:35:39 +0100

    Add files via upload

--- src/Content/Types/ColorPickerCustomTest.php
+++ src/Content/Types/ColorPickerCustomTest.php
@@ -6,8 +6,8 @@ use Sulu\Component\Content\SimpleContentType;
 
 class ColorPickerCustomTest extends SimpleContentType
 {
-    public function __construct()
+    public function __construct(string $name)
     {
-        parent::__construct('color_picker_custom_test');
+        parent::__construct($name);
     }
 }
--- src/DependencyInjection/ColorPickerCustomExtension.php
+++ src/DependencyInjection/ColorPickerCustomExtension.php
@@ -4,16 +4,26 @@ namespace Tommysonsylverstone\ComposerTest\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension as Extensions;
 
 /**
  * SecurityHeadersExtension
  */
 class ColorPickerCustomExtension extends Extension
 {
+    protected $test = new Extensions;
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        foreach ($config as $key => $value) {
+            $container->setParameter('security_headers.' . $key, $value);
+        }
+        $this->test->addAnnotatedClassesToCompile([
+            'Tommysonsylverstone\ComposerTest\Content\Types\ColorPickerCustomTest',
+        ]);
     }
 }
