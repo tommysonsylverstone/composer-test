f56d11c51d115818ec892cf853d26aa48acf01e9
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2021-12-08 15:35:48 +0100

    Add files via upload

--- src/DependencyInjection/ColorPickerCustomExtension.php
+++ src/DependencyInjection/ColorPickerCustomExtension.php
@@ -15,10 +15,5 @@ class ColorPickerCustomExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-        foreach ($config as $key => $value) {
-            $container->setParameter('security_headers.' . $key, $value);
-        }
     }
 }
